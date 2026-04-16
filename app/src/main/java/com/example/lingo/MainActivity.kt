e1c0b6168e926a4b1853c0479588b900736f8b9d
John-TRI1 <jtrichardsontri@gmail.com>
2026-04-16 14:25:14 -0500

    Added in properly updated files

--- app/src/main/java/com/example/lingo/MainActivity.kt
+++ app/src/main/java/com/example/lingo/MainActivity.kt
@@ -1,20 +1,14 @@
 package com.example.lingo
 
-
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.cardview.widget.CardView
 
-
-
-
-
 class MainActivity : ComponentActivity() {
-
 
     private var showingFront = true
 
@@ -22,6 +16,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val startQuizBtn = findViewById<View>(R.id.startQuizBtn)
+        val frontWord = findViewById<TextView>(R.id.frontWord)
+        val backWord = findViewById<TextView>(R.id.backWord)
+        val frontSentence = findViewById<TextView>(R.id.frontSentence)
+        val backSentence = findViewById<TextView>(R.id.backSentence)
+
+        startQuizBtn.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            intent.putExtra(QuizActivity.EXTRA_FLASHCARD_WORD, frontWord.text.toString())
+            intent.putExtra(QuizActivity.EXTRA_FLASHCARD_TRANSLATION, backWord.text.toString())
+            intent.putExtra(QuizActivity.EXTRA_FLASHCARD_SENTENCE, frontSentence.text.toString())
+            intent.putExtra(QuizActivity.EXTRA_TRANSLATED_SENTENCE, backSentence.text.toString())
+            startActivity(intent)
+        }
 
         val flashcard = findViewById<CardView>(R.id.flashcard)
         val flashcardFront = findViewById<View>(R.id.flashcard_front)
